b25df2aaf09f59ab804175a671ee326ee51a3778
root <root@10-9-116-33.(none)>
2017-05-18 10:41:55 +0800

    no cred test

---
+++ src/access_without_pathconf.c
@@ -0,0 +1,122 @@
+#include "common/frame_driver.h"
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#ifdef HAVE_POLL_H
+#include <poll.h>
+#endif
+
+#ifdef HAVE_NETINET_IN_H
+#include <netinet/in.h>
+#endif
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+#include <nfsc/libnfs.h>
+#include <nfsc/libnfs-raw.h>
+#include <nfsc/libnfs-raw-mount.h>
+#include <nfsc/libnfs-raw-nfs.h>
+#include <nfsc/libnfs-raw-portmap.h>
+#include <nfsc/libnfs-raw-rquota.h>
+
+
+
+struct sattr3 TESTCASE1_OBJATTR = {
+    .uid.set_it = 1,
+    .uid.set_uid3_u.uid = 1,
+    .gid.set_it = 1,
+    .gid.set_gid3_u.gid = 1,
+    .size.set_it = 1,
+    .size.set_size3_u.size = 222
+};
+
+struct sattr3 TESTCASE2_OBJATTR = {
+    .uid.set_it = 1,
+    .uid.set_uid3_u.uid = 0,
+    .gid.set_it = 1,
+    .gid.set_gid3_u.gid = 0,
+    .size.set_it = 1,
+    .size.set_size3_u.size = 333
+};
+
+time_t CREATE_VERF = 0;
+
+PATHCONF3resok ACCESS_PATHCONF;
+
+void nfs_access_testcase_final_cb(struct rpc_context *rpc, int status, void *data, void *private_data) {
+
+    struct client *client = private_data;
+
+    if (status == RPC_STATUS_ERROR) {
+        fprintf(stderr, "nfs/access call TESTCASE ACCESS NOCRED failed with \"%s\"\n", (char *)data);
+        exit(10);
+    }
+    if (status != RPC_STATUS_SUCCESS) {
+        fprintf(stderr, "nfs/access call TESTCASE ACCESS NOCRED to server %s failed, status:%d\n", client->server, status);
+        exit(10);
+    }
+
+    fprintf(stdout, "TESTCASE ACCESS NOCRED: Got reply from server for NFS/ACCESS procedure.\n");
+
+    ACCESS3res *res = data;
+
+    if (res->status == NFS3ERR_ACCES) {
+       fprintf(stdout, "TESTCASE ACCESS NOCRED: ACCESS NOCRED PASSED!\n\n");   
+    } else {
+       fprintf(stderr, "TESTCASE ACCESS NOCRED: ACCESS NOCRED FAILED: %d\n\n", res->status);
+    }
+    
+	client->is_finished = 1;
+}
+
+
+
+void nfs_access_testcase_prepare_cb(struct rpc_context *rpc, int status, void *data, void *private_data) {
+
+    struct client *client = private_data;
+
+    if (status != RPC_STATUS_SUCCESS) {
+        fprintf(stderr, "connection to RPC.NFSD on server %s failed\n", client->server);
+        exit(10);
+    }
+
+    fprintf(stdout, "Connected to RPC.NFSD on %s:%d\n", client->server, 2049);
+
+    struct ACCESS3args args;
+    memset((void*)&args, 0, sizeof(args));
+    args.object = client->rootfh;
+    args.access = ACCESS3_READ | ACCESS3_LOOKUP | ACCESS3_MODIFY | ACCESS3_EXECUTE;
+
+    int ret = rpc_nfs3_access_async(rpc, nfs_access_testcase_final_cb, &args, client);
+    if (ret) {
+        fprintf(stderr, "Failed to send access request|ret:%d\n", ret);
+        exit(10);
+    }
+}
+
+int main(int argc, char *argv[]) {
+
+    if (argc < 3) {
+        printf("usage: %s %s %s\n", argv[0], "server_ip", "mnt_path");
+        exit(10);
+    }
+
+    struct client client;
+    client.server = argv[1];
+    client.export = argv[2];
+    client.test_case_cb = nfs_access_testcase_prepare_cb; 
+
+    memset(&ACCESS_PATHCONF, 0, sizeof(PATHCONF3resok));
+
+	struct rpc_context *rpc = rpc_init_context();
+	rpc_set_auth(rpc, authnone_create());
+
+    drive_frame_with_rpc(client, rpc);
+
+    return 0;
+}
